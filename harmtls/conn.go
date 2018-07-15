5e8cf8f4de8423a78f09977832894adbe00597a2
Koichi Nakanishi <keizo042dev@gmail.com>
2018-07-15 18:12:22 +0900

    Remove dup

--- harmtls/conn.go
+++ harmtls/conn.go
@@ -1,42 +1,37 @@
 package harmtls
 
-//   enum {
-//          invalid(0),
-//          change_cipher_spec(20),
-//          alert(21),
-//          handshake(22),
-//          application_data(23),
-//          (255)
-//      } ContentType;
-
+// ContentType is TLS Record ContentType
 type ContentType int8
 
 const (
-	ContentTypeInvalid         ContentType = 0
-	ContentTypeChangeCuoerSpec             = 20
-	ContentTypeAlert                       = 21
-	ContentTypeHandshake                   = 22
-	ContentTypeHandshale                   = 23
+	// LegacyRecordVersion is a const value for backward compability
+	LegacyRecordVersion uint16 = 0x0303
 )
 
+// ContentTypes are defined at Section 5.1 of draft-ietf-tls-tls13-28.
+const (
+	ContentTypeInvalid          ContentType = 0
+	ContentTypeChangeChiperSpec             = 20
+	ContentTypeAlert                        = 21
+	ContentTypeHandshake                    = 22
+	ContentTypeHandshale                    = 23
+)
+
+// TLSPlaintext is a unencrypted payload.
 type TLSPlaintext struct {
 	LegacyRecordVersion uint16 // hard code value
 	Length              uint16
 	Fragment            []byte
 }
 
+// TLSInnerPlaintext is a TLS 1.3 record format.
 type TLSInnerPlaintext struct {
 	Content     []byte
 	ContentType ContentType
 	Zeros       uint8
 }
 
-type TLSInnerPlaintext struct {
-	Content []byte
-	Typ     ContentType
-	Zeros   []uint8
-}
-
+// TLSCiphertext is a encyrypted payload.
 type TLSCiphertext struct {
 	OpaqueType          ContentType
 	LegacyRecordVersion ProtocolVersion
