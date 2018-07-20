3f89364d888f03636f4d0d32cb57e4e1c33ac8ab
Koichi Nakanishi <keizo042dev@gmail.com>
2018-07-21 02:20:08 +0900

    Fix test

--- harmtls/record_test.go
+++ harmtls/record_test.go
@@ -8,25 +8,28 @@ import (
 // TODO(keizo042): Add test Plaintext/Ciphertext as table driven test
 
 func TestTLSPlaintextByte(t *testing.T) {
-	record := TLSPlaintext{
+	recordBefore := TLSPlaintext{
 		ContentType:         LegacyContentType,
 		LegacyRecordVersion: LegacyRecordVersion,
 		Length:              255,
 		Fragment:            make([]byte, 255),
 	}
 	buffer := new(bytes.Buffer)
-	if err := record.writeBuffer(buffer); err != nil {
+	if err := recordBefore.writeBuffer(buffer); err != nil {
 		t.Fatal(err)
 	}
+	recordAfter := new(TLSPlaintext)
+	if err := recordAfter.readBuffer(buffer); err != nil {
+		t.Fatal(err)
+	}
+	if recordBefore.ContentType != recordAfter.ContentType {
+		t.Fatalf("expected %d, actual %d", recordBefore.ContentType, recordAfter.ContentType)
+	}
+	if recordBefore.LegacyRecordVersion != recordAfter.LegacyRecordVersion {
+		t.Fatalf("expected %d, actual %d", recordBefore.LegacyRecordVersion, recordAfter.LegacyRecordVersion)
+	}
+	if recordBefore.Length != recordAfter.Length {
+		t.Fatalf("expected %d, actual %d", recordBefore.Length, recordAfter.Length)
+	}
 
-	if buffer.Len() != 260 {
-		t.Fatalf("buffer length: expected %d, actual %d", 260, buffer.Len())
-	}
-	expectedHeader := []byte{0x17, 0x03, 0x03, 0x00, 0xff}
-	actualHeader := buffer.Bytes()[:len(expectedHeader)]
-	for i := range expectedHeader {
-		if expectedHeader[i] != actualHeader[i] {
-			t.Fatalf("buffer element %d: expected 0x%x, actual 0x%x", i+1, expectedHeader[i], actualHeader[i])
-		}
-	}
 }
